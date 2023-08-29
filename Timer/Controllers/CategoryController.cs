e924c3928f8807014a748a899a62ff1885bd6599
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-08-29 15:41:02 +0200

    Finish Category CRUD

---
+++ Timer/Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Timer.Models;
+using Timer.Models.Data;
+
+namespace Timer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id <= 0 || id == null)
+            {
+                return BadRequest();
+            }
+
+            Category category = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Category> categoryList = _db.Categories.ToList();
+            return Json(new { data = categoryList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            if (id <= 0 || id == null)
+            {
+                return BadRequest();
+            }
+
+            Category category = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (category == null)
+            {
+                return Json(new { success = false, message = "Error while deleting!" });
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Category deleted successfully!" });
+        }
+
+
+        #endregion
+    }
+}
---
+++ Timer/wwwroot/js/category.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function() {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblCategories').DataTable({
+        "ajax": { url: '/category/getall' },
+        "columns": [
+            { data: 'name', "width": "15%" },
+            { data: 'description', "width": "30%" },
+            { data: 'billable', "width": "10%" },
+            { data: 'active', "width": "10%" },
+            { data: 'vat', "width": "10%" },
+            {
+                data: 'id',
+                "render": function(data) {
+                    return `<div class="w-75 btn-group" role="group">
+                    <a href="/category/edit?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                    <a onClick=Delete('/category/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#2c3e50',
+        confirmButtonText: 'Delete Category'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    })
+}

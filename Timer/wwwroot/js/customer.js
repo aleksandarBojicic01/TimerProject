a0ae28765c39251d7e3574aa8865259fe12aeb5b
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-08-30 09:39:31 +0200

    Finish Customer CRUD

--- Timer/wwwroot/js/customer.js
+++ Timer/wwwroot/js/customer.js
@@ -1,49 +1,41 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    dataTable = $('#tblCategories').DataTable({
-        "ajax": { url: '/category/getall' },
-        "columnDefs" : [
+    dataTable = $('#tblCustomers').DataTable({
+        "ajax": { url: '/customer/getall' },
+        "columnDefs": [
             {
-                "targets": [2, 3],
+                "targets": [6],
                 "className": "text-center",
                 "orderable": false,
             }
         ],
         "columns": [
             { data: 'name', "width": "15%" },
-            { data: 'description', "width": "30%" },
+            { data: 'code', "width": "5%" },
+            { data: 'address', "width": "15%" },
+            { data: 'city', "width": "10%" },
+            { data: 'email', "width": "10%" },
+            { data: 'phone', "width": "10%"},
             {
-                data: 'billable',
+                data: 'active',
                 "render": function (data) {
                     if (data === true) {
                         return '<input type="checkbox" checked name="id" onclick="return false" class="m-auto" style="vertical-align: middle; position: relative; ">'
                     }
                     return '<input type="checkbox" name="id" onclick="return false" class="m-auto">'
-                
-            },
+
+                },
                 "width": "10%"
             },
-          {
-              data: 'active',
-              "render": function (data) {
-                  if (data === true) {
-                      return '<input type="checkbox" checked name="id" onclick="return false" class="m-auto" style="vertical-align: middle; position: relative; ">'
-                  }
-                  return '<input type="checkbox" name="id" onclick="return false" class="m-auto">'
-
-              },
-              "width": "10%"
-          },
-            { data: 'vat', "width": "10%" },
             {
                 data: 'id',
-                "render": function(data) {
+                "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                    <a href="/category/edit?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
-                    <a onClick=Delete('/category/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                    <a href="/customer/edit?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>
+                    <a onClick=Delete('/customer/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
                     </div>`
                 },
                 "width": "25%"
@@ -60,7 +52,7 @@ function Delete(url) {
         showCancelButton: true,
         confirmButtonColor: '#d33',
         cancelButtonColor: '#2c3e50',
-        confirmButtonText: 'Delete Category'
+        confirmButtonText: 'Delete Customer'
     }).then((result) => {
         if (result.isConfirmed) {
             $.ajax({

41c1e4c76bebdea8e23f11ca4ee8c09d3e7c5709
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-08-30 09:51:26 +0200

    Add read operation for Users

---
+++ Timer/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Timer.Models;
+using Timer.Models.Data;
+
+namespace Timer.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<IdentityUser> users = _db.Users.ToList();
+            return View(users);
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<IdentityUser> users = _db.Users.ToList();
+            return Json(new { data = users });
+        }
+
+        #endregion
+    }
+}

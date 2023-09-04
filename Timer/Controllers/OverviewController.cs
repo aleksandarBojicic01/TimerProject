830ee1731401a8f679278baaa552f56f91168335
pejicdj <djordjepejic999@gmail.com>
2023-09-04 10:27:38 +0200

    add overview

---
+++ Timer/Controllers/OverviewController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Timer.Controllers.ViewModels;
+using Timer.Models.Data;
+using TimeLog = Timer.Models.TimeLog;
+namespace Timer.Controllers
+{
+    public class OverviewController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public OverviewController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var OverviewVM = new OverviewVM
+            {
+                TimeLogs = _db.TimeLogs.ToList(),
+                Customers = _db.Customers.ToList(),
+                Categories = _db.Categories.ToList(),
+                Tasks = _db.Tasks.ToList(),
+                Users = _db.Users.ToList()
+            };
+            return View(OverviewVM);
+        }
+        [HttpPost]
+        public IActionResult Index(OverviewVM viewModel)
+        {
+            var timeLogsQuery = _db.TimeLogs.AsQueryable();
+
+            if (!string.IsNullOrEmpty(viewModel.SelectedCustomer))
+            {
+                timeLogsQuery = timeLogsQuery.Where(t => t.CustomerId.ToString() == viewModel.SelectedCustomer);
+            }
+
+            if (!string.IsNullOrEmpty(viewModel.SelectedCategory))
+            {
+                timeLogsQuery = timeLogsQuery.Where(t => t.CategoryId.ToString() == viewModel.SelectedCategory);
+            }
+
+            if (viewModel.StartDate.HasValue || viewModel.EndDate.HasValue)
+            {
+                timeLogsQuery = timeLogsQuery.Where(t =>
+                                   (!viewModel.StartDate.HasValue || t.EndDate >= viewModel.StartDate) &&
+                                                      (!viewModel.EndDate.HasValue || t.StartDate <= viewModel.EndDate));
+            }
+
+            if (!string.IsNullOrEmpty(viewModel.SelectedUser))
+            {
+                timeLogsQuery = timeLogsQuery.Where(t => t.IdentityUserId == viewModel.SelectedUser);
+            }
+
+            viewModel.TimeLogs = timeLogsQuery.ToList();
+            viewModel.Customers = _db.Customers.ToList();
+            viewModel.Categories = _db.Categories.ToList();
+            viewModel.Tasks = _db.Tasks.ToList();
+            viewModel.Users = _db.Users.ToList();
+
+            return View(viewModel);
+        }
+
+        public IActionResult TimeLog()
+        {
+            var OverviewVM = new OverviewVM
+            {
+                TimeLogs = _db.TimeLogs.ToList(),
+                Customers = _db.Customers.ToList(),
+                Categories = _db.Categories.ToList(),
+                Tasks = _db.Tasks.ToList(),
+                Users = _db.Users.ToList()
+            };
+            return View();
+        }
+        [HttpPost]
+        public IActionResult TimeLog(OverviewVM viewModel)
+        {
+            var timeLogsQuery = _db.TimeLogs.AsQueryable();
+
+            if (!string.IsNullOrEmpty(viewModel.SelectedCustomer))
+            {
+                timeLogsQuery = timeLogsQuery.Where(t => t.CustomerId.ToString() == viewModel.SelectedCustomer);
+            }
+
+            if (!string.IsNullOrEmpty(viewModel.SelectedCategory))
+            {
+                timeLogsQuery = timeLogsQuery.Where(t => t.CategoryId.ToString() == viewModel.SelectedCategory);
+            }
+
+            if (viewModel.StartDate.HasValue || viewModel.EndDate.HasValue)
+            {
+                timeLogsQuery = timeLogsQuery.Where(t =>
+                                                  (!viewModel.StartDate.HasValue || t.EndDate >= viewModel.StartDate) &&
+                                                                                                       (!viewModel.EndDate.HasValue || t.StartDate <= viewModel.EndDate));
+            }
+
+            if (!string.IsNullOrEmpty(viewModel.SelectedUser))
+            {
+                timeLogsQuery = timeLogsQuery.Where(t => t.IdentityUserId == viewModel.SelectedUser);
+            }
+
+            viewModel.TimeLogs = timeLogsQuery.ToList();
+            viewModel.Customers = _db.Customers.ToList();
+            viewModel.Categories = _db.Categories.ToList();
+            viewModel.Tasks = _db.Tasks.ToList();
+            viewModel.Users = _db.Users.ToList();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Timer/Controllers/OverviewVM.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Timer.Models;
+
+namespace Timer.Controllers.ViewModels
+{
+    public class OverviewVM
+    {
+       
+        public List<TimeLog> TimeLogs { get; set; }
+        public DateTime? StartDate { get;internal set; }
+        public DateTime? EndDate { get; internal set; }
+        public List<Customer> Customers { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Models.Task> Tasks { get; set; }
+        public List<IdentityUser> Users { get; set; }
+        public string SelectedCustomer { get; internal set; }
+        public string SelectedCategory { get; internal set; }
+        public string Hasvalue { get; internal set; }
+     
+        public string SelectedUser { get; internal set; }
+    }
+}

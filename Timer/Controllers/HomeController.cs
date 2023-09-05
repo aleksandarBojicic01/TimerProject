eb7d0d77be305a75ba569c3e0bb99aab65192c67
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-09-05 15:04:47 +0200

    add auto logging, needs more testing

--- Timer/Controllers/HomeController.cs
+++ Timer/Controllers/HomeController.cs
@@ -2,7 +2,11 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Timer.Models;
+using Timer.Models.Data;
+using Timer.Models.ViewModels;
 using Timer.Utility;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Security.Claims;
 
 namespace Timer.Controllers
 {
@@ -10,15 +14,76 @@ namespace Timer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+        private static LoggerVM? vm;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<SelectListItem> CategoryList = _db.Categories.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            IEnumerable<SelectListItem> CustomerList = _db.Customers.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            IEnumerable<SelectListItem> TaskList = _db.Tasks.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+
+            var loggerVM = new LoggerVM
+            {
+                TimeLog = new TimeLog(),
+                Tasks = TaskList,
+                Categories = CategoryList,
+                Customers = CustomerList
+            };
+            if (vm == null)
+            {
+                vm = loggerVM;
+            }
+
+            return View(vm);
+        }
+        [HttpPost]
+        public void StartTimer([FromBody]LoggerVM passedVm)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            passedVm.TimeLog.UserId = userId;
+            passedVm.TimeLog.Date = DateTime.Now;
+            passedVm.TimeLog.StartTime = DateTime.Now.TimeOfDay;
+            vm = passedVm;
+            TempData["success"] = "Timer started!";
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoggerVM passedVm)
+        {
+            if (vm != null)
+            {
+                vm.TimeLog.EndTime = DateTime.Now.TimeOfDay;
+                vm.TimeLog.Duration = vm.TimeLog.EndTime - vm.TimeLog.StartTime;
+
+                if (ModelState.IsValid)
+                {
+                    _db.TimeLogs.Add(vm.TimeLog);
+                    _db.SaveChanges();
+                    TempData["success"] = "Time Log entry added!";
+                    vm = null;
+                }
+            }
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Timer/wwwroot/js/log.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    $("#button-start").click(function () {
+        var customerId = $("#cust-select").val(); // Assuming cust-select is the ID of the customer select element
+        var categoryId = $("#cat-select").val(); // Assuming cat-select is the ID of the category select element
+        var taskId = $("#task-select").val(); // Assuming task-select is the ID of the task select element
+        var billable = $("#TimeLog_Billable").is(":checked");
+        var notes = $("#notesarea").val();
+        var url = $(this).data("url");
+
+        // Prepare the data to send in the request body
+        var dataToSend = {
+            TimeLog: {
+                CustomerId: customerId,
+                CategoryId: categoryId,
+                TaskId: taskId,
+                Billable: billable,
+                Notes: notes
+                // Add other properties here
+            }
+        };
+
+        console.log('JSON Data:', JSON.stringify(dataToSend));
+
+        $.ajax({
+            url: url, // Replace with the actual URL
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(dataToSend),
+            success: function (response) {
+                // Handle success response, if needed
+                console.log('Timer started successfully.');
+                // Optionally, update the UI to reflect the timer has started
+            },
+            error: function (error) {
+                // Handle error, if needed
+                console.error('Error starting the timer:', error);
+            }
+        });
+    });
+
+    // Other JavaScript code...
+});
